1abb7a8ae4c55d8c4926ccc61f97b77654fbaaa9
Natalia_Go <gorshana@gmail.com>
2023-03-26 17:46:41 +0300

    task1

--- Program.cs
+++ Program.cs
@@ -3,39 +3,86 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
-// metka;
-System.Console.Write("Введите положительное число N: ");
-int n = Convert.ToInt32(Console.ReadLine());
 
-System.Console.Write("Введите положительное число M: ");
-int m = Convert.ToInt32(Console.ReadLine());
+ metka:
 
+ System.Console.Write("Введите положительное число N: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ System.Console.Write("Введите положительное число M: ");
+ int m = Convert.ToInt32(Console.ReadLine());
 
-// if (n>0)&(m>0);
-{
-    if (n > m)
-    {
-        int sum = 0;
-        for (int i = m; i <= n; i++)
-        {
+ if (m<0||n<0)
+ {
+  System.Console.WriteLine("числа не положительные, повторите ввод");
+  goto metka;
+ }
+
+ if (n>0&&m>0);
+ {
+     if (n > m)
+     {
+         int sum = 0;
+         for (int i = m; i <= n; i++)
+         {
+             sum = sum + i;
+         }
+         System.Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} равна {sum}");
+     }
+     else if (m >= n)
+     {
+         int sum = 0;
+         for (int i = n; i <= m; i++)
+         {
             sum = sum + i;
-        }
-        System.Console.WriteLine(
-            $"Сумма натуральных элементов в промежутке от {m} до {n} равна {sum}"
-        );
-    }
-    else if (m >= n)
-    {
-        int sum = 0;
-        for (int i = n; i <= m; i++)
-        {
-            sum = sum + i;
-        }
-        System.Console.WriteLine(
-            $"Сумма натуральных элементов в промежутке от {n} до {m} равна {sum}"
-        );
-    }
-}
+         }
+         System.Console.WriteLine(
+             $"Сумма натуральных элементов в промежутке от {n} до {m} равна {sum}");
+     }
+ }
+
+
+
+
+// System.Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+//Sum(m, n);
+
+// void Sum(int numM, int numN)
+// {
+// if (numM < numN)
+// {
+// return;
+// }
+// Sum(numM, numN + 1);
+// System.Console.Write(numN + );
+// }
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// // M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// // M = 4; N = 8 -> "4, 6, 7, 8"
+
+// System.Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Print(m, n);
+
+// void Print(int numM, int numN)
+// {
+// if (numN < numM)
+// {
+// return;
+// }
+// Print(numM, numN - 1);
+// System.Console.Write(numN + "\t");
+// }
+
+
+
+
+
 // }
 // else
 //  {
@@ -131,3 +178,23 @@ int m = Convert.ToInt32(Console.ReadLine());
 // // //  Даны два неотрицательных числа m и n.
 // // // m = 2, n = 3 -> A(m,n) = 9
 // // // m = 3, n = 2 -> A(m,n) = 29
+
+// If m=0
+// {
+//     A=n+1;
+// }
+// Else If m>0&n==0
+// {A=(m-1,1)}
+// Else if m>0&n>0
+// {A(m-1, A(m,n-1))}
+
+// int A(int n, int m)
+// {
+//   if (n == 0)
+//     return m + 1;
+//   else
+//     if ((n != 0) && (m == 0))
+//       return A(n - 1, 1);
+//     else
+//       return A(n - 1, A(n, m - 1));
+// }
